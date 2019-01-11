a31716de5d44cb4a2ab5512ad539416d67b1026e
Pi2f <pierre.franc.gap@gmail.com>
2019-01-11 10:10:04 +0100

    mplab integration (2)

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 module.exports = {
     'port' : "3000",
-    'urlDB' : 'mongodb://tu-streames1:tu-streames1@ds153974.mlab.com:53974/tu-streames',
+    'urlDB' : 'mongodb://192.168.99.100:32777/TuStreames',
+    // 'urlDB' : 'mongodb://tu-streames1:tu-streames@ds153974.mlab.com:53974/tu-streames',
+    // 'urlDB' : 'mongodb://database/TuStreames',
     vimeo: {
         clientID : "52661bbe93423155c914afc02a3fef458e29975e",
         unauthenticatedToken: "79745e50364d3998b30466e00e7d122e",
         secret: "8HfB7YG/WAWcX/5Tz5Vrx7VuuYYCjn1YT35JgU6Pj7Ncr4RxVdTQxzu2CY2HfNqQEzvh4rqh/d6df0JZFZ25c5FlntePqIOuLsOwRfzLVSmjutwlDkMvyeNGs2UsbQ/w"
+
     }
 }
