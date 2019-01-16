329cceef191728347332856c098b2a935cdd227b
Pi2f <pierre.franc.gap@gmail.com>
2019-01-16 15:59:36 +0100

    update

--- script.bat
+++ script.bat
@@ -3,10 +3,10 @@ title TuStreames Application
 mode con cols=80 lines=16
 color 0F
 
-cd %USERPROFILE%/Developpement/TuStreames/
+cd %USERPROFILE%/Developpement/TuStreames/apps/app
 start npm start
 
-cd user
+cd ../user
 start npm start
 
 cd ../playlist
